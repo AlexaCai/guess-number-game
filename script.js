5545149a2b6d96e094773ceab012ee350916889e
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2024-02-01 21:08:23 -0500

    Code refactoring - functions creation

--- script.js
+++ script.js
@@ -1,63 +1,71 @@
 'use strict';
 
-// Define the secret number to find outside the event code block since we only want to define it once, and not everytime a user click on the check button
-let secretNumber = Math.trunc(Math.random() * 10) + 1
-// document.querySelector('.number').textContent = secretNumber
-
-// Users initial score, set to 'score' variable
-let score = 10;
+// Define the secretNumber outside the event code block since we only want to define it once, and not everytime a user click on the check button
+let secretNumber = Math.trunc(Math.random() * 100) + 1
+let score = 100;
 let highScore = 0;
 
+// Functions created to avoid writing the same lines of code several times for the same purpose
+const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message
+}
+
+const displaySecretNumber = function (number) {
+    document.querySelector('.number').textContent = number
+}
+
+const displayScore = function (score) {
+    document.querySelector('.score').textContent = score
+}
+
+const secretNumberMargin = function (margin) {
+    document.querySelector('.number').style.width = margin;
+}
+
+const backgroundColor = function (color) {
+    document.querySelector('body').style.backgroundColor = color;
+}
+
 document.querySelector('.check').addEventListener('click', function () {
-    //Important to convert to number because by default the input value is a string, which is not best to perform number comparaison
+    //Important to convert the entry to a number because by default the input value is a string, which is not good to perform number comparaison in condition logics below
     const guess = Number(document.querySelector('.guess').value);
 
     // When there's no input number
     if (!guess) {
-        document.querySelector('.message').textContent = 'No number ⛔'
+        displayMessage('No number ⛔')
 
         // When users have the right answer
     } else if (guess === secretNumber) {
-        document.querySelector('.message').textContent = 'Correct number! 🎉';
-        document.querySelector('.number').textContent = secretNumber
-        document.querySelector('body').style.backgroundColor = '#60b347';
-        document.querySelector('.number').style.width = '30rem';
+        displayMessage('Correct number! 🎉');
+        displaySecretNumber(secretNumber);
+        secretNumberMargin('30rem');
+        backgroundColor('#60b347');
         if (score > highScore) {
             highScore = score;
             document.querySelector('.highscore').textContent = highScore;
         }
 
-        // When users guess too high
-    } else if (guess > secretNumber) {
+        // When users have the wrong answer
+    } else if (guess !== secretNumber) {
         if (score > 1) {
-            document.querySelector('.message').textContent = 'Number too high ⬆️';
-            score = score - 1
-            document.querySelector('.score').textContent = score;
+            displayMessage(guess > secretNumber ? 'Number too high ⬆️' : 'Number too low ⬇️');
+            score = score - 2
+            displayScore(score);
         } else {
-            document.querySelector('.message').textContent = 'You lost the game🥺';
-            document.querySelector('.score').textContent = 0;
-        }
-
-        // When users guess too low
-    } else if (guess < secretNumber) {
-        if (score > 1) {
-            document.querySelector('.message').textContent = 'Number too low ⬇️';
-            score = score - 1
-            document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = 'You lost the game🥺';
-            document.querySelector('.score').textContent = 0;
+            displayMessage('You lost the game🥺');
+            displayScore(0);
         }
     }
 });
 
+// Set everything back to default when users click reset
 document.querySelector('.again').addEventListener('click', function () {
-    score = 10;
-    secretNumber = Math.trunc(Math.random() * 10) + 1
-    document.querySelector('body').style.backgroundColor = '#222';
-    document.querySelector('.number').style.width = '15rem';
-    document.querySelector('.number').textContent = '?'
-    document.querySelector('.message').textContent = 'Start guessing...';
+    score = 100;
+    secretNumber = Math.trunc(Math.random() * 100) + 1
     document.querySelector('.guess').value = '';
-    document.querySelector('.score').textContent = score;
+    backgroundColor('#222');
+    secretNumberMargin('15rem');
+    displaySecretNumber('?');
+    displayMessage('Start guessing...');
+    displayScore(score);
 })
